86f14a147812235412654f358beaf3949fee25e1
IvanKogut11 <55024892+IvanKogut11@users.noreply.github.com>
2019-10-20 20:05:52 +0500

    Add files via upload

---
+++ ownChatBot/JavaBot/src/games/Anagrams.java
@@ -0,0 +1,23 @@
+package games;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Anagrams {
+	
+	private static ArrayList<String> words = new ArrayList<String>();
+	
+	public Anagrams()
+	{
+		
+	}
+	
+	public void Run()
+	{
+		
+	}
+}
